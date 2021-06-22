5d13383319cbc9e849e088f644b430b1f2c5cc5e
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-06-22 08:46:39 +0200

    WIP

--- init.lua
+++ init.lua
@@ -72,3 +72,23 @@ require('lsp.elixir-ls')
 
 -- USER CFG
 vim.cmd[[set guifont=Hack\ Nerd\ Font\ Mono:h17]]
+
+
+require'FTerm'.setup({
+    dimensions  = {
+        height = 0.8,
+        width = 0.8,
+        x = 0.5,
+        y = 0.5
+    },
+    border = 'single' -- or 'double'
+})
+
+-- Keybinding
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>', opts)
+map('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
+
+vim.cmd[[nnoremap <leader>H :lua require('spectre').open()<CR>]]
